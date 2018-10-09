b7c0a07833ccbd9b7690f64f780da010c19d430c
Andrew Cymmer <acymmer@holmanenterprises.com>
2018-10-09 10:52:37 -0400

    Made dockerfile a bit leaner.
    Moved compilation of nominatim to a build image.

--- 3.2/init.sh
+++ 3.2/init.sh
@@ -15,13 +15,13 @@ sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nomin
 sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
 useradd -m -p password1234 nominatim && \
-chown -R nominatim:nominatim ./src && \
+chown -R nominatim:nominatim /app/src && \
 echo "[INFO] init.sh - running first setup.php on osm-file ..." && \
-sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
+sudo -u nominatim /app/src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
 echo "[INFO] init.sh - running second setup.php with import-tiger-data ..." && \
-sudo -u nominatim ./src/build/utils/setup.php --import-tiger-data && \
+sudo -u nominatim /app/src/build/utils/setup.php --import-tiger-data && \
 echo "[INFO] init.sh - running third and last setup.php to create the functions, enable diff updates, and create partition functions ..." && \
-sudo -u nominatim ./src/build/utils/setup.php --create-functions --enable-diff-updates --create-partition-functions && \
+sudo -u nominatim /app/src/build/utils/setup.php --create-functions --enable-diff-updates --create-partition-functions && \
 # sudo -u postgres psql postgres -tAc "CREATE INDEX nodes_index ON public.planet_osm_ways USING gin (nodes);"
 sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR stop && \
 sudo chown -R postgres:postgres /data/$PGDIR
