2f9cdca011abe9e2f323ad963e7997e46b38a52e
Andrew Cymmer <acymmer@holmanenterprises.com>
2018-12-27 13:56:55 -0500

    Attempting to fix missing user during OSM load.
    Moved the major activities from init.sh to the load-osmMoved the major activities from init.sh to the load-osm.sh script.

--- 3.2/init.sh
+++ 3.2/init.sh
@@ -14,13 +14,6 @@ chown postgres:postgres /data/$PGDIR
 export  PGDATA=/data/$PGDIR  && \
 sudo -u postgres /usr/lib/postgresql/9.5/bin/initdb -D /data/$PGDIR && \
 sudo -u postgres cp /etc/postgresql/9.5/main/postgresql.conf /data/$PGDIR && \
-sudo -u postgres cp /etc/postgresql/9.5/main/pg_hba.conf /data/$PGDIR && \
-sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR start && \
-sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
-sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
-useradd -m -p password1234 nominatim && \
-chown -R nominatim:nominatim /app/src && \
-sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR stop && \
-sudo chown -R postgres:postgres /data/$PGDIR
+sudo -u postgres cp /etc/postgresql/9.5/main/pg_hba.conf /data/$PGDIR 
 
 echo "[INFO] init.sh - finished setting up empty postgres database. Next step is to either run load-osm.sh , or run psql restore with a backup file."
--- 3.2/load-osm.sh
+++ 3.2/load-osm.sh
@@ -24,7 +24,11 @@ echo "[INFO] Importing OSM data file '$OSMFILE'..."
 export  PGDATA=/data/$PGDIR  && \
 sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR start && \
 sleep 2 && \
+sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -u postgres createuser -s nominatim && \
+sudo -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='www-data'" | grep -q 1 || sudo -u postgres createuser -SDR www-data && \
 sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
+useradd -m -p password1234 nominatim && \
+chown -R nominatim:nominatim /app/src && \
 sudo -u nominatim /app/src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
 # sudo -u postgres psql postgres -tAc "CREATE INDEX nodes_index ON public.planet_osm_ways USING gin (nodes);"
 sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR stop && \
